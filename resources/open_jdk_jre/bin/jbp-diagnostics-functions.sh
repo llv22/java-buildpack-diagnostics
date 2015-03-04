8cd3c36123745b7eb415c4e198966d8cbdb50cb7
Lari Hotari <lhotari@users.noreply.github.com>
2015-03-04 15:53:43 -0500

    calculate presigned url for downloading OOM heapdump from s3

--- resources/open_jdk_jre/bin/jbp-diagnostics-functions.sh
+++ resources/open_jdk_jre/bin/jbp-diagnostics-functions.sh
@@ -41,21 +41,34 @@ upload_stdin_to_s3() {
       https://${s3Bucket}.s3.amazonaws.com/${filename}
 }
 
+calculate_presigned_s3_url() {
+    filename="$1"
+    s3Bucket=$JBPDIAG_AWS_BUCKET
+    s3Key=$JBPDIAG_AWS_ACCESS_KEY
+    s3Secret=$JBPDIAG_AWS_SECRET_KEY
+    resource="/${s3Bucket}/$filename"
+    expires=`date --date="+48 hours" +"%s"`
+    stringToSign="GET\n\n\n${expires}\n${resource}"
+    signature=`echo -en ${stringToSign} | openssl sha1 -hmac ${s3Secret} -binary | base64`
+    echo "https://${s3Bucket}.s3.amazonaws.com/${filename}?AWSAccessKeyId=${s3Key}&Expires=${expires}&Signature=${signature}"
+}
+
 upload_oom_heapdump_to_s3() {
     usetempfile="$1"
     heapdumpfile=$PWD/oom_heapdump.hprof
     if [[ -e $heapdumpfile && -n "$JBPDIAG_AWS_BUCKET" ]]; then
         filename="oom_heapdump_$(date +"%s").hprof.gz"
+        s3_presign_url=`calculate_presigned_s3_url $filename`
         if [[ $usetempfile == 1 ]]; then
             # usage of temporary file is allowed
             echo "Compressing $heapdumpfile"
             gzip $heapdumpfile
-            echo "Uploading to S3"
+            echo "Uploading to S3. Presigned access url: $s3_presign_url"
             upload_to_s3 ${heapdumpfile}.gz $filename && rm ${heapdumpfile}.gz
         else
             echo "Calculating compressed size first to minimize disk space usage"
             gzippedsize=`cat $heapdumpfile | gzip -c | wc -c | awk '{print $1}'`
-            echo "Compressing and uploading $gzippedsize bytes to S3"
+            echo "Compressing and uploading $gzippedsize bytes to S3. Presigned access url: $s3_presign_url"
             cat $heapdumpfile | gzip -c | upload_stdin_to_s3 $filename $gzippedsize && rm $heapdumpfile
         fi
     fi
