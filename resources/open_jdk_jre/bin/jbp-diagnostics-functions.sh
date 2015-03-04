c654a4016f2b844c6144a9d2de47293528621cf6
Lari Hotari <lhotari@users.noreply.github.com>
2015-03-04 14:34:01 -0500

    add function for uploading files to Amazon S3

---
+++ resources/open_jdk_jre/bin/jbp-diagnostics-functions.sh
@@ -0,0 +1,22 @@
+# based on blog post code http://tmont.com/blargh/2014/1/uploading-to-s3-in-bash
+upload_to_s3() {
+    filepath="$1"
+    filename="$2"
+    if [[ -z "$filename" ]]; then
+        filename=$(basename "$filepath")
+    fi
+    s3Bucket=$JBPDIAG_AWS_BUCKET
+    s3Key=$JBPDIAG_AWS_ACCESS_KEY
+    s3Secret=$JBPDIAG_AWS_SECRET_KEY
+    contentType="application/octet-stream"
+    resource="/${s3Bucket}/$filename"
+    dateValue=`date -R`
+    stringToSign="PUT\n\n${contentType}\n${dateValue}\n${resource}"
+    signature=`echo -en ${stringToSign} | openssl sha1 -hmac ${s3Secret} -binary | base64`
+    curl -X PUT -T "${filepath}" \
+      -H "Host: ${s3Bucket}.s3.amazonaws.com" \
+      -H "Date: ${dateValue}" \
+      -H "Content-Type: ${contentType}" \
+      -H "Authorization: AWS ${s3Key}:${signature}" \
+      https://${s3Bucket}.s3.amazonaws.com/${filename}
+}
