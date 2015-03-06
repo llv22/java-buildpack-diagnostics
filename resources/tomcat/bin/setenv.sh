266cdb6d66becc25eed5767cd0fef7bae1217b27
Lari Hotari <lhotari@users.noreply.github.com>
2015-03-06 18:22:41 -0500

    log info about environment to files at startup time

--- resources/tomcat/bin/setenv.sh
+++ resources/tomcat/bin/setenv.sh
@@ -1,3 +1,6 @@
 echo "Creating /home/vcap/.grails-static-website"
 mkdir /home/vcap/.grails-static-website
 
+$JAVA_HOME/bin/java -XshowSettings:all -XX:+PrintFlagsFinal -version > /home/vcap/logs/java_flags.log 2> /home/vcap/logs/java_info.log
+env > /home/vcap/logs/env.log
+ulimit -a > /home/vcap/logs/ulimit.log
